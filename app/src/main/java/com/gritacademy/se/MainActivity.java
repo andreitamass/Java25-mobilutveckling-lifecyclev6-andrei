9e12f62c2a8ec99431d1af34c5030adbb24dc0ee
andreitamas <andrei.tamass83@gmail.com>
2026-09-14 12:30:08 +0200

    first commit

--- app/src/main/java/com/gritacademy/se/MainActivity.java
+++ app/src/main/java/com/gritacademy/se/MainActivity.java
@@ -1,20 +1,58 @@
 package com.gritacademy.se;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText usernameText;
+    EditText passwordText;
+    EditText buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        usernameText = findViewById(R.id.usernameText);
+        passwordText = findViewById(R.id.passwordText);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameText.getText().toString();
+                String password = passwordText.getText().toString();
+
+                if(username.equals("admin") &&  password.equals("test")) {
+                          startActivity(new Intent(MainActivity.this, MainActivity2.class));
+                } else {
+                    Toast.makeText(MainActivity.this, "Wrong input", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
