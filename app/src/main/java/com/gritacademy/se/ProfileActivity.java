6a97fc5d0730f4c9d89a4072e546bd24b370a411
andreitamas <andrei.tamass83@gmail.com>
2026-09-14 13:37:15 +0200

    added height code in activity, list for 99 ages

--- app/src/main/java/com/gritacademy/se/ProfileActivity.java
+++ app/src/main/java/com/gritacademy/se/ProfileActivity.java
@@ -8,13 +8,64 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    private EditText profileName;
+    private SeekBar profileHeight;
+    private EditText profileWeight;
+    private Spinner profileAge;
+    private RadioButton profileGenderMan;
+    private RadioButton profileGenderWoman;
+    private RadioButton profileGenderOther;
+    TextView heightView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
+
+        profileName = findViewById(R.id.profileName);
+        profileHeight = findViewById(R.id.profileHeight);
+        profileWeight = findViewById(R.id.profileWeight);
+
+        profileAge = findViewById(R.id.profileAge);
+        profileGenderMan = findViewById(R.id.profileGenderMan);
+        profileGenderWoman = findViewById(R.id.profileGenderWoman);
+        profileGenderOther = findViewById(R.id.profileGenderOther);
+        heightView = findViewById(R.id.textView);
+
+        profileHeight.setMax(80);
+        profileHeight.setProgress(40);
+
+        String[] ages = new String[99]; // 99 åldrar
+
+        for (int i = 0; i < ages.length; i++) {
+            ages[i] = String.valueOf(i + 18);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, ages
+        );
+
+        profileAge.setAdapter(adapter);
+
+
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
